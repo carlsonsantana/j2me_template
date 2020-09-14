df88ace6688d92cd1b2035533d52e4ac4cee93e8
Carlson Santana Cruz <carlsonsantana@gmail.com>
2020-09-13 23:19:25 -0300

    Initial commit

---
+++ src/main/java/org/groupid/artifactid/MainMIDlet.java
@@ -0,0 +1,27 @@
+package org.groupid.artifactid;
+
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.Display;
+import javax.microedition.lcdui.Displayable;
+import javax.microedition.midlet.MIDlet;
+import javax.microedition.midlet.MIDletStateChangeException;
+
+public class MainMIDlet extends MIDlet {
+    public MainMIDlet() {
+        /*Intialise*/
+    }
+
+    /*Enters MIDlet into active state*/
+    protected void startApp()  {
+        /*Do all your getDisplay() , setCurrent(), and other one time initialization*/
+    }
+
+    /*Enters MIDlet into pause state*/
+    protected void pauseApp() {}
+
+    /*Enters MIDlet into destroy state*/
+    protected void destroyApp(boolean unconditional) throws MIDletStateChangeException {
+        notifyDestroyed();
+    }
+}
